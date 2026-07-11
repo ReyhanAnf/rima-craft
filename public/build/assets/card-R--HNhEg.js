c0cffe21ab95c3597c333809205eb3e1942cb7dc
Reyhan Andrea Firdaus <reyhananf7@gmail.com>
2026-07-11 14:58:20 +0700

    feature/TMP Livewire Fixing

---
+++ public/build/assets/card-R--HNhEg.js
@@ -0,0 +1,32 @@
+import{A as e,N as t,l as n,s as r,u as i,w as a}from"./runtime-core.esm-bundler-4I4cuEBR.js";import{i as o}from"./app-CcJc8GCk.js";import{t as s}from"./basecomponent-D3okuxfa.js";var c={name:`Card`,extends:{name:`BaseCard`,extends:s,style:o.extend({name:`card`,style:`
+    .p-card {
+        background: dt('card.background');
+        color: dt('card.color');
+        box-shadow: dt('card.shadow');
+        border-radius: dt('card.border.radius');
+        display: flex;
+        flex-direction: column;
+    }
+
+    .p-card-caption {
+        display: flex;
+        flex-direction: column;
+        gap: dt('card.caption.gap');
+    }
+
+    .p-card-body {
+        padding: dt('card.body.padding');
+        display: flex;
+        flex-direction: column;
+        gap: dt('card.body.gap');
+    }
+
+    .p-card-title {
+        font-size: dt('card.title.font.size');
+        font-weight: dt('card.title.font.weight');
+    }
+
+    .p-card-subtitle {
+        color: dt('card.subtitle.color');
+    }
+`,classes:{root:`p-card p-component`,header:`p-card-header`,body:`p-card-body`,caption:`p-card-caption`,title:`p-card-title`,subtitle:`p-card-subtitle`,content:`p-card-content`,footer:`p-card-footer`}}),provide:function(){return{$pcCard:this,$parentInstance:this}}},inheritAttrs:!1};function l(o,s,c,l,u,d){return e(),i(`div`,a({class:o.cx(`root`)},o.ptmi(`root`)),[o.$slots.header?(e(),i(`div`,a({key:0,class:o.cx(`header`)},o.ptm(`header`)),[t(o.$slots,`header`)],16)):n(``,!0),r(`div`,a({class:o.cx(`body`)},o.ptm(`body`)),[o.$slots.title||o.$slots.subtitle?(e(),i(`div`,a({key:0,class:o.cx(`caption`)},o.ptm(`caption`)),[o.$slots.title?(e(),i(`div`,a({key:0,class:o.cx(`title`)},o.ptm(`title`)),[t(o.$slots,`title`)],16)):n(``,!0),o.$slots.subtitle?(e(),i(`div`,a({key:1,class:o.cx(`subtitle`)},o.ptm(`subtitle`)),[t(o.$slots,`subtitle`)],16)):n(``,!0)],16)):n(``,!0),r(`div`,a({class:o.cx(`content`)},o.ptm(`content`)),[t(o.$slots,`content`)],16),o.$slots.footer?(e(),i(`div`,a({key:1,class:o.cx(`footer`)},o.ptm(`footer`)),[t(o.$slots,`footer`)],16)):n(``,!0)],16)],16)}c.render=l;export{c as t};
